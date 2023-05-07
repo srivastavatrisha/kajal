f6a6a6dea4b237defd2395ebca0ba087d6c2c97a
Trisha srivastava <123794254+srivastavatrisha@users.noreply.github.com>
2023-05-07 23:57:14 +0530

    Add files via upload

---
+++ tictac.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       printf("----- Welcome to my game world --------\n");
+       printf("*****TIC TAC TOE GAME*****\n");
+       printf("\n");      
+       char index[]={'1','2','3','4','5','6','7','8','9'};
+       int a;
+       char X;
+       printf("          1           |         2        |         3         \n");
+       printf("----------------------|------------------|------------------ \n");
+       printf("          4           |         5        |         6         \n");
+       printf("----------------------|------------------|-------------------\n");
+       printf("          7           |         8        |         9         \n");
+       printf("\n");
+    
+       printf("mark   your  move ");
+       scanf("%d",&a);
+       index[a-1]='X';
+       
+       printf("      1     |      2      |      3      \n",index[0],index[1],index[2]);
+       printf("------------|-------------|--------------\n");
+       printf("      4     |      5      |      6       \n",index[3],index[4],index[5]);
+       printf("------------|-------------|--------------\n");
+       printf("      7     |      8      |      9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("      1    |      2       |         3     \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("      4    |      5       |         6     \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("      7    |      8       |         9      \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1    |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("----------|--------------|---------------\n");
+       printf("     4    |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("----------|--------------|---------------\n");
+       printf("     7    |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+    
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+       
+       
+       printf("     1     |      2       |       3      \n",index[0],index[1],index[2]);
+       printf("-----------|--------------|---------------\n");
+       printf("     4     |      5       |       6       \n",index[3],index[4],index[5]);
+       printf("-----------|--------------|---------------\n");
+       printf("     7     |      8       |       9       \n",index[6],index[7],index[8]);
+       printf("\n");
+}    
+      
